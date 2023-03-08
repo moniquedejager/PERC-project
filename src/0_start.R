09fb7cc06ca19d0b3488fa0dc0124d3a94d171b6
Monique de Jager <m.dejager@uu.nl>
2023-03-08 09:22:46 +0100

    created a new start R-file

---
+++ src/0_start.R
@@ -0,0 +1,15 @@
+# This R-script contains all the elements needed for this project: 
+# 1. The individual-based model that generates camera-trapping events
+# 2. The script to turn these data into presence/absence matrices
+# 3. The royle-nichols model that is run on many combinations of:
+#    - study duration
+#    - number of camera traps
+#    - time interval size
+#    - density at location 1
+#    - density at location 2
+# 4. The analysis of the royle-nichols results
+# 5. The application of the analysis results on real data
+
+# 1. The individual-based model that generates camera-trapping events #####
+source('1_IBM.R')
+
