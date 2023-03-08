fe884ba17592e9a0ef89d2eef4ee2a05ab18ac78
Monique de Jager <m.dejager@uu.nl>
2023-03-08 14:21:23 +0100

    created new R-script that generates presence/absence matrices per species per camera per day

---
+++ src/4.1_create_pres_abs_matrices.R
@@ -0,0 +1,69 @@
+library(lubridate)
+m1 <- read.table('./data/raw/FSC and nonFSC data/Congo_data_2021_09_10.csv', 
+                 sep=',', header=TRUE)
+m2 <- read.table('./data/raw/FSC and nonFSC data/Gabon_data_2021_09_09.csv', 
+                 sep=',', header=TRUE)
+m <- rbind(m1, m2)
+
+# how many cameras? And what's their survey period?
+n_cams <- length(unique(m$Unique_CT_Name))
+max_effort <- max(m$Effort..days..New)
+max_effort_per_cam <- tapply(m$Effort..days..New, m$Unique_CT_Name, max)
+
+# we create a template, which can be used for each species:
+pres_abs_template <- matrix(NA, n_cams, max_effort)
+for (i in 1:n_cams){
+  pres_abs_template[i, 1:max_effort_per_cam[i]] <- 0
+}
+cov <- data.frame(cam = sort(unique(m$Unique_CT_Name)), 
+                           cluster = tapply(m$Cluster, m$Unique_CT_Name, min),
+                           site_type = NA)
+for (i in cov$cam){
+  cov$site_type[cov$cam == i] <- m$Type.of.Site[m$Unique_CT_Name == i][1]
+}
+write.table(cov, './data/processed/FSC and nonFSC data/covariates.txt', 
+            append=FALSE, col.names = TRUE, row.names = FALSE)
+
+# we will only focus on mammals in this study:
+m <- m[m$Class == "MAMMALIA",]
+m <- m[m$Species != "",]
+m <- m[m$Species != "unknown",]
+m <- m[m$Species != "unknown ",]
+
+genus <- gsub(' ','',m$Genus)
+species <- gsub(' ', '', m$Species)
+species <- paste(genus, species, sep=' ')
+n <- tapply(species, species, length)
+
+# write data to new input files, as summarized per species per camera per day:
+uSpec <- sort(unique(species))
+for (spec in uSpec)
+{
+  if (n[uSpec == spec] > 150)
+  {
+    m2 <- m[species == spec,]
+    
+    date1 <- as.Date(m2$Camera.Start.Date.New)
+    date2 <- as.Date(m2$Photo.Date.New)
+    x <- interval(ymd(date1),ymd(date2))
+    m2$x <- x %/% days(1)
+    
+    group <- paste(m2$Unique_CT_Name, m2$x, sep='-')
+    i <- 1:length(group)
+    i <- tapply(i, group, min)
+    
+    m3 <- m2[i,]
+    pres_abs <- pres_abs_template
+
+    for (i in 1:length(m3$x)) {
+      pres_abs[m3$Unique_CT_Name[i] == cov$cam, m3$x[i]] <- 1
+    }
+    
+    filename <- paste('./data/processed/FSC and nonFSC data/pres_abs', 
+                      spec, '.txt')
+    write.table(pres_abs, filename, 
+                row.names=FALSE, col.names=FALSE, append=FALSE)
+  }  
+}
+
+
--- src/4_compare_FSC_and_nonFSC_data.R
+++ src/4_compare_FSC_and_nonFSC_data.R
@@ -1,1 +1,6 @@
-# './src/4_compare_FSC_and_nonFSC_data.R'
+# To compare the camera trapping events between FSC and non-FSC forests,
+# we need to do the following:
+
+# 4.1: Create presence/absence matrices per species, per day, and
+# per camera trap:
+source('./src/4.1_create_pres_abs_matrices.R')
