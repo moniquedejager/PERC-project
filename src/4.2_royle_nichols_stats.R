eac7991a4b4473ad2901a76a3db6193b16888072
Monique de Jager <m.dejager@uu.nl>
2023-03-24 13:51:01 +0100

    changed the stats script to only examine the RN model with the optimal time interval size and dT = 5 days

--- src/4.2_royle_nichols_stats.R
+++ src/4.2_royle_nichols_stats.R
@@ -54,6 +54,9 @@ royle_nichols_stats = function(i){
       estimate_interval_size(survey_effort, Ppresence, j)[2]
   }
   
+  optimal_interval_size <- df$optimal_interval_size[(df$survey_effort*df$z) == 
+                                                      max(df$survey_effort*df$z)]
+  
   tiff(filename=paste('./results/figures/FSC and nonFSC/optimal interval size', spec, '.tiff'), 
        height=5, width=5, units='in', res=300)
   coeff <- 1/3000
@@ -73,68 +76,108 @@ royle_nichols_stats = function(i){
     geom_vline(xintercept=optimal_interval_size, linetype='dashed')
   dev.off()
   
-  # create the output file:
-  x <- vector(length=0)
-  data <- data.frame(species = x, 
-                     time_interval = x, 
-                     study_duration = x, 
-                     n_cams = x, 
-                     z = x, 
-                     P = x, 
-                     p_presence = x)
-  outputfile <- paste('./results/output/FSC and nonFSC data/royle_nichols_stats', 
-                      spec, '.txt')
-  if (!file.exists(outputfile)) { 
-    write.table(data, outputfile, col.names = TRUE, row.names = FALSE)
+  time_interval <- optimal_interval_size
+  # create new dets-matrix with the right time intervals:
+  n_intervals <- floor(200/time_interval)
+  stu_dur <- n_intervals*time_interval
+  dets2 <- dets[,1:stu_dur]
+  dets3 <- matrix(0, 455, n_intervals)
+  x <- sort(rep(1:n_intervals, time_interval))
+  for (i3 in 1:455) {
+    dets3[i3,] <- tapply(dets2[i3,], x, max)
   }
   
-  for (time_interval in 1:100) {
-    # create new dets-matrix with the right time intervals:
-    n_intervals <- floor(200/time_interval)
-    stu_dur <- n_intervals*time_interval
-    dets2 <- dets[,1:stu_dur]
-    dets3 <- matrix(0, 455, n_intervals)
-    x <- sort(rep(1:n_intervals, time_interval))
-    for (i3 in 1:455) {
-      dets3[i3,] <- tapply(dets2[i3,], x, max)
-    }
+  study_duration <- optimal_interval_size*2  
     
-    for (study_duration in (2:(200/time_interval))*time_interval){
-      # we need to remove all camera traps with effort < study_duration:
-      n_intervals <- (study_duration/time_interval)
-      dets4 <- dets3[cov$Effort >= study_duration,1:n_intervals]
-      cov2 <- cov[cov$Effort >= study_duration,]
+  # we need to remove all camera traps with effort < study_duration:
+  n_intervals <- (study_duration/time_interval)
+  dets4 <- dets3[cov$Effort >= study_duration,]
+  cov2 <- cov[cov$Effort >= study_duration,]
+  
+  Ppresence <- df$Ppresence[(df$survey_effort*df$z) == 
+                             max(df$survey_effort*df$z)]
+  survey_effort <- df$survey_effort[(df$survey_effort*df$z) == 
+                                  max(df$survey_effort*df$z)]
+  
+  umf <- unmarkedFrameOccu(y=dets4, siteCovs=cov2)
       
-      p_presence <- mean(rowSums(dets4) > 0)
-      umf <- unmarkedFrameOccu(y=dets4, siteCovs=cov2)
-      
-      if (length(unique(cov$Cluster)) == 1) {
-        mod <- occuRN(~Visibility - 1 ~SiteType, umf)
-      } else {
-        mod <- occuRN(~Visibility - 1 ~Cluster + SiteType, umf)
-      }
-      
-      s1 <- summary(mod)
-      
-      # sometimes the model has too many covariates and cannot compute all 
-      # the parameters. We should then fall back on a simpler model: 
-      if (is.na(s1$state[length(s1$state$Estimate),3])){
-        mod <- occuRN(~Visibility - 1 ~SiteType, umf)
-        s1 <- summary(mod)
-      }
-      
-      data <- data.frame(species = spec, 
-                         time_interval = time_interval, 
-                         study_duration = study_duration, 
-                         n_cams = length(cov2$Cam), 
-                         z = s1$state[length(s1$state$Estimate),3], 
-                         P = s1$state[length(s1$state$Estimate),4], 
-                         p_presence=p_presence)
-      write.table(data, outputfile, 
-                  append=TRUE, col.names = FALSE, row.names = FALSE)
-      
-    }
+  if (length(unique(cov$Cluster)) == 1) {
+    mod <- occuRN(~Visibility - 1 ~SiteType, umf)
+  } else {
+    mod <- occuRN(~Visibility - 1 ~Cluster + SiteType, umf)
   }
+      
+  s1 <- summary(mod)
+      
+  # sometimes the model has too many covariates and cannot compute all 
+  # the parameters. We should then fall back on a simpler model: 
+  if (is.na(s1$state[length(s1$state$Estimate),3])){
+    mod <- occuRN(~Visibility - 1 ~SiteType, umf)
+    s1 <- summary(mod)
+  }
+      
+  outputfile <- paste('./results/output/FSC and nonFSC data/royle_nichols_stats', 
+                      spec, '.txt')
+  
+  data <- data.frame(species = spec, 
+                     time_interval = time_interval, 
+                     survey_effort = survey_effort, 
+                     n_cams = length(cov2$Cam), 
+                     z = s1$state[length(s1$state$Estimate),3], 
+                     P = s1$state[length(s1$state$Estimate),4], 
+                     p_presence=Ppresence,
+                     type='optimal dT')
+  write.table(data, outputfile, 
+              append=FALSE, col.names = TRUE, row.names = FALSE)
+  
+  # with dT = 5:
+  time_interval <- 5
+  n_intervals <- floor(200/time_interval)
+  stu_dur <- n_intervals*time_interval
+  dets2 <- dets[,1:stu_dur]
+  dets3 <- matrix(0, 455, n_intervals)
+  x <- sort(rep(1:n_intervals, time_interval))
+  for (i3 in 1:455) {
+    dets3[i3,] <- tapply(dets2[i3,], x, max)
+  }
+  
+  study_duration <- 10  
+  
+  # we need to remove all camera traps with effort < study_duration:
+  n_intervals <- (study_duration/time_interval)
+  dets4 <- dets3[cov$Effort >= study_duration,]
+  cov2 <- cov[cov$Effort >= study_duration,]
+  
+  Ppresence <- df$Ppresence[df$max_interval_size == 10]
+  survey_effort <- df$survey_effort[df$max_interval_size == 10]
+  
+  umf <- unmarkedFrameOccu(y=dets4, siteCovs=cov2)
+  
+  if (length(unique(cov$Cluster)) == 1) {
+    mod <- occuRN(~Visibility - 1 ~SiteType, umf)
+  } else {
+    mod <- occuRN(~Visibility - 1 ~Cluster + SiteType, umf)
+  }
+  
+  s1 <- summary(mod)
+  
+  # sometimes the model has too many covariates and cannot compute all 
+  # the parameters. We should then fall back on a simpler model: 
+  if (is.na(s1$state[length(s1$state$Estimate),3])){
+    mod <- occuRN(~Visibility - 1 ~SiteType, umf)
+    s1 <- summary(mod)
+  }
+
+  data2 <- data.frame(species = spec, 
+                     time_interval = time_interval, 
+                     survey_effort = survey_effort, 
+                     n_cams = length(cov2$Cam), 
+                     z = s1$state[length(s1$state$Estimate),3], 
+                     P = s1$state[length(s1$state$Estimate),4], 
+                     p_presence=Ppresence,
+                     type='5 day interval')
+  write.table(data2, outputfile, 
+              append=TRUE, col.names = FALSE, row.names = FALSE)
 }
 
 i = 1:35 # there are 35 species that we want to analyse
