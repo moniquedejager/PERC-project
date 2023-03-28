d291fd72f37ae14de4dffc86d62c2f7d8c2759a6
Monique de Jager <m.dejager@uu.nl>
2023-03-28 11:34:14 +0200

    doubled the survey effort, as cams were present at both location A and B. Also added the relative performance figure.

--- src/3_royle_nichols_analysis.R
+++ src/3_royle_nichols_analysis.R
@@ -64,7 +64,7 @@ df2 = df[!is.infinite(df$logitPpres),]
 
 y <- df2$z
 x1 <- df2$dT 
-x2 <- 1/(df2$StudyDuration * df2$nCams)
+x2 <- 1/(df2$StudyDuration * df2$nCams * 2)
 x3 <- df2$logitPpres
 
 mod0 <- lm(y~x1 + x2 + x3 + 
@@ -87,6 +87,7 @@ mod0 <- lm(y~x1 + x2 + x3 +
              I(x2^4):I(x3^4) + 
              I(x1^4):I(x2^4):I(x3^4))
 summary(mod0)
+# R2 = 0.54
 
 s = summary(mod0)
 
@@ -190,139 +191,34 @@ ggplot(df3[sel,], aes(x=original_SD, y=Ppres, fill=dT)) +
   ylab('Ppresence')+
   theme(legend.position = "top") 
 
-# With study duration and number of cameras separately:
-y <- df3$z
-x1 <- df3$StudyDuration
-x2 <- df3$nCams
-x3 <- df3$logitPpres
-
-mod0 <- lm(y~x1 + x2 + x3 + 
-           x1:x2 + x1:x3 +  
-           x2:x3 +
-           x1:x2:x3 + 
-           
-           I(x1^2) + I(x2^2) + I(x3^2) + 
-           I(x1^2):I(x2^2) + I(x1^2):I(x3^2) + 
-           I(x2^2):I(x3^2) +
-           I(x1^2):I(x2^2):I(x3^2) +  
-           
-           I(x1^3) + I(x2^3) + I(x3^3) + 
-           I(x1^3):I(x2^3) + I(x1^3):I(x3^3) + 
-           I(x2^3):I(x3^3) + 
-           I(x1^3):I(x2^3):I(x3^3) + 
-           
-           I(x1^4) + I(x2^4) + I(x3^4) + 
-           I(x1^4):I(x2^4) + I(x1^4):I(x3^4) + 
-           I(x2^4):I(x3^4) + 
-           I(x1^4):I(x2^4):I(x3^4))
-summary(mod0)
-
-mod1 <- lm(y~x1 + x2 + x3 + 
-             x1:x2 + x1:x3 +  
-             x2:x3 +
-             x1:x2:x3 + 
-             
-             I(x1^2) + I(x2^2) + I(x3^2) + 
-             I(x1^2):I(x2^2) + I(x1^2):I(x3^2) + 
-             I(x2^2):I(x3^2) +
-             
-             I(x1^3) + I(x2^3) + I(x3^3) + 
-             I(x1^3):I(x2^3) + I(x1^3):I(x3^3) + 
-             I(x1^3):I(x2^3):I(x3^3) + 
-             
-             I(x1^4) + I(x2^4) + I(x3^4) + 
-             I(x1^4):I(x2^4) + I(x1^4):I(x3^4) + 
-             I(x2^4):I(x3^4) + 
-             I(x1^4):I(x2^4):I(x3^4))
-summary(mod1)
-
-df3$est_z = mod1$fitted.values
-
-y <- df3$dT
-mod2 <- glm(y~x1 + x2 + x3 + 
-             x1:x2 + x1:x3 +  
-             x2:x3 +
-             x1:x2:x3 + 
-             
-             I(x1^2) + I(x2^2) + I(x3^2) + 
-             I(x1^2):I(x2^2) + I(x1^2):I(x3^2) + 
-             
-             I(x1^3) + I(x2^3) + I(x3^3) + 
-             I(x1^3):I(x2^3) + I(x1^3):I(x3^3) + 
-             I(x2^3):I(x3^3) + 
-             I(x1^3):I(x2^3):I(x3^3) + 
-             
-             I(x1^4) + I(x2^4) + I(x3^4) + 
-             I(x1^4):I(x2^4) + I(x1^4):I(x3^4) + 
-             I(x1^4):I(x2^4):I(x3^4), family='poisson')
-s = summary(mod2)
-with(summary(mod2), 1 - deviance/null.deviance)
-# R2 = 0.38
-df3$est_dT = ceiling(mod2$fitted.values)
-
-sel = (df3$nCams %in% c(5, 10, 15, 20, 25)) & (df3$est_z > 1.96)
-df3$SD2 <- round(log(df3$original_SD)/log(365),1)
-
-p1 <- ggplot(df3[sel,], aes(x=SD2, y=Ppres, fill=est_z)) +
-  geom_raster() + 
-  scale_x_continuous(breaks = c(0.4, 0.6, 0.8, 1), labels=c(10, 30, 100, 365)) +
-  facet_wrap(vars(nCams), ncol=1) + 
-  scale_fill_continuous(type='viridis', name='Estimated z-value') + 
-  xlab('Sampling period (days)') + 
-  ylab('Ppresence')+
-  theme(legend.position = "top") 
-
-p2 <- ggplot(df3[sel,], aes(x=SD2, y=Ppres, fill=est_dT)) +
-  geom_raster() + 
-  #scale_y_continuous(trans='log10') + 
-  scale_x_continuous(breaks = c(0.4, 0.6, 0.8, 1), labels=c(10, 30, 100, 365)) +
-  facet_wrap(vars(nCams), ncol=1) + 
-  scale_fill_continuous(type='viridis', trans='log10',name='Optimal interval size (days)') + 
-  xlab('Sampling period (days)') + 
-  ylab('Ppresence')+
-  theme(legend.position = "top") 
-
-windows(height=10, width=5)
-ggarrange(p1, p2 + rremove("ylab"))
-
-write.table(s$coefficients, 
-            './results/output/simulations/to_estimate_time_interval_sizes_I.txt', 
-            append=FALSE, row.names = FALSE, col.names=TRUE)
-
 # with study duration and camera numbers combined:
 y <- df3$z
-x1 <- df3$StudyDuration*df3$nCams
+x1 <- 1/(df3$StudyDuration*df3$nCams*2)
 x2 <- df3$logitPpres
 
-mod0 <- lm(y~x1 + x2 + 
-             x1:x2 + 
-             
-             I(x1^2) + I(x2^2) + 
-             I(x1^2):I(x2^2) +   
-             
-             I(x1^3) + I(x2^3) + 
-             I(x1^3):I(x2^3) +  
-             
-             I(x1^4) + I(x2^4) + 
-             I(x1^4):I(x2^4) + 
-             
-             I(x1^5) + I(x2^5) + 
-             I(x1^5):I(x2^5) +
-             
-             I(x1^6) + I(x2^6) + 
-             I(x1^6):I(x2^6) )
-summary(mod0)
+mod <- lm(y~x1)
+summary(mod)
+mod1 <- lm(y~x1+x2)
+summary(mod1)
+mod2 <- lm(y~x1*x2 + I(x2^2)*x1 + I(x2^3)*x1 + I(x2^4))
+summary(mod2)
+mod3 <- lm(y~x1*x2 + I(x2^2)*x1 + I(x2^3)*x1 + I(x2^4)*x1)
+summary(mod3)
+anova(mod2, mod3)
 
-df3$sampling_effort <- df3$StudyDuration * df3$nCams
-df3$est_z <- mod0$fitted.values
+# R2 = 0.76
+
+df3$sampling_effort <- df3$StudyDuration * df3$nCams * 2
+df3$est_z <- mod3$fitted.values
 
 sel = (df3$est_z > 1.96)
-sel = !is.na(df3$z)
+#sel = !is.na(df3$z)
 
 labs = c(50, 150, 400, 1000, 3000, 10000)
 p1 <- ggplot(df3[sel,], aes(x=round(log(sampling_effort), 1), y=Ppres, fill=est_z)) +
   geom_raster() + 
-  scale_x_continuous(breaks = log(labs), labels=labs) +
+  ylim(0,1) + 
+  scale_x_continuous(breaks = log(labs), labels=labs, limits=c(4,10)) +
   scale_fill_continuous(type='viridis', name='Estimated z-value') + 
   xlab('Sampling effort (total # camera days)') + 
   ylab(expression(paste('Proportion of cameras with detections (', P[presence], ')')))+
@@ -330,39 +226,152 @@ p1 <- ggplot(df3[sel,], aes(x=round(log(sampling_effort), 1), y=Ppres, fill=est_
 
 y <- df3$dT
 
-mod1 <- glm(y~x1 + x2 + 
-             x1:x2 + 
-             
-             I(x1^2) + I(x2^2) + 
-             I(x1^2):I(x2^2) +   
-             
-             I(x1^3) + I(x2^3) + 
-             I(x1^3):I(x2^3) +  
-             
-             I(x1^4) + I(x2^4) + 
-             I(x1^4):I(x2^4) + 
-             
-             I(x1^5) + I(x2^5) + 
-             I(x1^5):I(x2^5) +
-             
-             I(x1^6) + I(x2^6) + 
-             I(x1^6):I(x2^6), family='poisson')
-s = summary(mod1)
+mod <- glm(y~x1, family='poisson')
+summary(mod)
+mod1 <- glm(y~x1+x2, family='poisson')
+summary(mod1)
+mod2 <- glm(y~x1*x2 + I(x2^2)*x1 + I(x2^3)*x1 + I(x2^4), family='poisson')
+summary(mod2)
+mod3 <- glm(y~x1*x2 + I(x2^2)*x1 + I(x2^3)*x1 + I(x2^4)*x1, family='poisson')
+summary(mod3)
 
-with(summary(mod1), 1 - deviance/null.deviance)
+s = summary(mod3)
 
-df3$est_dT = ceiling(mod1$fitted.values)
+with(summary(mod3), 1 - deviance/null.deviance)
+# R2 = 0.22
+
+df3$est_dT = ceiling(mod3$fitted.values)
 
 p2 <- ggplot(df3[sel,], aes(x=round(log(sampling_effort), 1), y=Ppres, fill=est_dT)) +
   geom_raster() + 
-  scale_x_continuous(breaks = log(labs), labels=labs) +
+  ylim(0, 1) + 
+  scale_x_continuous(breaks = log(labs), labels=labs, limits=c(4, 10)) +
   scale_fill_continuous(type='viridis', trans='log10', name='Optimal interval size (days)') + 
   xlab('Sampling effort (total # camera days)') + 
   ylab('')+
   theme(legend.position = "top") 
 
-ggarrange(p1, p2 + rremove("ylab"))
+ggarrange(p1, p2 + rremove("ylab"), labels=c('A', 'B'))
 
-write.table(s$coefficients, 
-            './results/output/simulations/to_estimate_time_interval_sizes_II.txt', 
-            append=FALSE, row.names = FALSE, col.names=TRUE)
+# what is the difference between dT = 5 and optimal time interval? 
+df4 <- df[df$dT == 5,]
+# per combination of survey effort and Ppresence, we calculate the 
+# percentage of RN models that resulted in a significant and correct
+# answer: 
+
+df4$sampling_effort <- df4$StudyDuration * df4$nCams * 2
+df4$samp_eff2 <- round(log(df4$sampling_effort), 1)
+df4$Ppres <- ceiling(df4$Ppresence*20)/20
+df4$z[is.na(df4$z)] <- 0
+
+group <- paste(df4$samp_eff2, df4$Ppres, sep='-')
+n_tot <- tapply(df4$z, group, length)
+n_correct <- tapply(df4$z > 1.96, group, sum)
+p_correct <- n_correct/n_tot *100
+sampling_effort <- tapply(df4$samp_eff2, group, mean)
+Ppresence <- tapply(df4$Ppres, group, mean)
+df5 <- data.frame(p_correct = p_correct,
+                  sampling_effort = sampling_effort,
+                  Ppresence = Ppresence)
+
+labs <- c(50, 150, 400, 1000, 3000, 10000)
+
+p4 <- ggplot(df5, aes(x=sampling_effort, y=Ppresence, fill=p_correct)) +
+  geom_raster() + 
+  scale_x_continuous(breaks = log(labs), labels=labs) +
+  scale_fill_continuous(type='viridis', name='% correct') + 
+  xlab('Sampling effort (total # camera days)') + 
+  ylab(expression(
+    paste('Proportion of cameras with detections (', P[presence], ')'))) +
+  theme(legend.position = "top") 
+
+df3$samp_eff2 <- round(log(df3$sampling_effort), 1)
+df3$z[is.na(df3$z)] <- 0
+
+group <- paste(df3$samp_eff2, df3$Ppres, sep='-')
+n_tot <- tapply(df3$z, group, length)
+n_correct <- tapply(df3$z > 1.96, group, sum)
+p_correct <- n_correct/n_tot *100
+sampling_effort <- tapply(df3$samp_eff2, group, mean)
+Ppresence <- tapply(df3$Ppres, group, mean)
+df6 <- data.frame(p_correct = p_correct,
+                  sampling_effort = sampling_effort,
+                  Ppresence = Ppresence)
+
+p5 <- ggplot(df6, aes(x=sampling_effort, y=Ppresence, fill=p_correct)) +
+  geom_raster() + 
+  scale_x_continuous(breaks = log(labs), labels=labs) +
+  scale_fill_continuous(type='viridis', name='% correct') + 
+  xlab('Sampling effort (total # camera days)') + 
+  ylab(expression(
+    paste('Proportion of cameras with detections (', P[presence], ')'))) +
+  theme(legend.position = "top") 
+
+df5$type <- 'Using 5 day interval'
+df6$type <- 'Using optimal interval size'
+df7 <- rbind(df5, df6)
+
+p6 <- ggplot(df7, aes(x=sampling_effort, y=Ppresence, fill=p_correct)) +
+  geom_raster() + 
+  scale_x_continuous(breaks = log(labs), labels=labs) +
+  scale_fill_continuous(type='viridis', name='% correct') + 
+  xlab('Sampling effort (total # camera days)') + 
+  ylab(expression(
+    paste('Proportion of cameras with detections (', P[presence], ')'))) +
+  facet_wrap(vars(type))
+
+# for each value in df2, find the corresponding value
+# in df4 and calculate the difference:
+group1 <- paste(df5$sampling_effort, df5$Ppresence, sep='-')
+group2 <- paste(df6$sampling_effort, df6$Ppresence, sep='-')
+
+for (i in unique(group1)){
+  p1 <- df5$p_correct[group1 == i]
+  p2 <- df6$p_correct[group2 == i]
+  if (length(p2) == 0){
+    p2 <- 0
+    x <- data.frame(p_correct =0,
+                    sampling_effort = df5$sampling_effort[group1 == i],
+                    Ppresence = df5$Ppresence[group1 == i],
+                    type = 'Using optimal interval size')
+    df7 <- rbind(df7, x)
+  } 
+  performance <- p2 / (p1 + p2)
+  if (is.na(performance)) { performance <- 0.5}
+  if (performance < 0.5) { performance <- 0.5}
+  x <- data.frame(p_correct = performance,
+                  sampling_effort = df5$sampling_effort[group1 == i],
+                  Ppresence = df5$Ppresence[group1 == i],
+                  type = 'Difference')
+  df7 <- rbind(df7, x)
+
+}
+
+sel <- df7$type != 'Difference'
+p7 <- ggplot(df7[sel,], aes(x=sampling_effort, y=Ppresence, fill=p_correct)) +
+  geom_raster() + 
+  scale_x_continuous(breaks = log(labs), labels=labs) +
+  scale_fill_continuous(type='viridis', name='% correct') + 
+  xlab('Sampling effort (total # camera days)') + 
+  ylab(expression(
+    paste('Proportion of cameras with detections (', P[presence], ')'))) +
+  facet_wrap(vars(type))
+
+sel <- df7$type == 'Difference'
+p3 <- ggplot(df7[sel,], aes(x=sampling_effort, y=Ppresence, fill=p_correct)) +
+  geom_raster() + 
+  ylim(0,1)+
+  scale_x_continuous(breaks = log(labs), labels=labs, limits=c(4, 10)) +
+  scale_fill_continuous(type='viridis', name='Relative performance') + 
+  xlab('Sampling effort (total # camera days)') + 
+  ylab(expression(
+    paste('Proportion of cameras with detections (', P[presence], ')'))) +
+  theme(legend.position = "top") 
+
+windows(height=5, width=12)
+tiff(filename='./results/figures/simulations/estimated_z_values_optimal_interval_sizes_and_relative_performance.tiff', 
+     height=5, width=12, units='in', res=300)
+ggarrange(p1, p2 + rremove("ylab"), p3 + rremove("ylab"), 
+          labels=c('A', 'B', 'C'), nrow=1)
+dev.off()
+
